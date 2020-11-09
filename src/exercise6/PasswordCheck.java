099cdfb9b85e5539610dc2afce9941ecfcb3678b
yoananb <yoana_blagova@abv.bg>
2020-11-09 19:55:11 +0000

    Ubung6

--- src/exercise6/PasswordCheck.java
+++ src/exercise6/PasswordCheck.java
@@ -5,5 +5,34 @@ import java.util.Scanner;
 public class PasswordCheck {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+
+        while(true){
+        System.out.println("Enter your password:");          
+
+          String password = input.nextLine();
+         boolean valid = true;
+         int digitCount = 0;
+         if (password.length()<8){
+           valid = false;
+           continue;
+         } else{
+           for(int i=0; i<password.length(); i++){
+            if (Character.isDigit(password.charAt(i))){
+              digitCount++;
+            }
+             if (!Character.isLetterOrDigit(password.charAt(i))){
+               valid = false;
+               break;
+             }
+           }
+         } if (digitCount<2){
+           valid = false;
+         } if(valid){
+            System.out.println("Password valid!");
+           break;
+         } else{
+           System.out.println("Password invalid!");
+         }
+        }
     }
 }
